70e15c8a6d3435b753ae7f2f7883331c06f6e612
Qudsia Aziz <qudsiya@gmail.com>
2011-12-06 14:32:19 -0500

    It seems to work.

--- publist.rb
+++ publist.rb
@@ -1,37 +1,60 @@
 #!/usr/bin/env ruby
 
-#puts "Hello"
 
-
-if ARGV[0]==""
-	dir = "./sample/"
+if ARGV.length < 1
+	puts "Please enter a path ./example/: "
+	dir = $stdin.gets.chomp
 else
 	dir = ARGV[0]
 end
 
-puts "\n\nShowing files on or after 20111202_094602 only\n\n"
+if ARGV.length < 2
+	puts "Please enter a date YYYYMMDD: "
+	date = $stdin.gets.chomp	
+	#date = 20111202
+else 
+	date = ARGV[1].to_i
+end
 
-#Dir.foreach("/Users/qudsia/Projects/publist/sample") do |entry|
+if ARGV.length < 3
+	puts "Please enter a time HHMMSS: "
+	time = $stdin.gets.chomp
+else 
+	time = ARGV[2].to_i
+end
+
+#dir = "./sample/"
+#time = 94602
+#date = 20111202
+
+links_array = Array.new
+
 Dir.foreach(dir) do |entry|
+  
   unless entry == "." or entry == ".." or entry == ".DS_Store"
   
-  	file_date = entry.gsub(/\s+/, "").split(/_/)
+  	file_name = entry.gsub(/\s+/, "").split(/_/)
+	 	
+  	if file_name[3].to_i >= date.to_i and file_name[4].to_i >= time.to_i
   	
-  	
-  	
-  	if file_date[3].to_i >= 20111202 and file_date[4].to_i >= 94602
-  	
-  		puts "\nFile name: " + entry
-  		puts "Beginning of file"
-  		puts "*******************************************"
+  		#puts "\nFile name: " + entry
+  		#puts "Beginning of file *******************************************"
   		file = File.open(dir + entry)
 		while (line = file.gets)
-			puts line
+			#puts line
+			links_array.push line unless /^#/.match(line) or line=="\n"
  	 	end
+		#puts "End of file *************************************************\n"
 	
-		puts "*******************************************"
-		puts "End of file\n"
 	
 	end
+	
   end
+  
 end
+
+#puts links_array.sort.uniq
+puts "\n\n\n"
+puts "Number of links found: #{links_array.length}" 
+
+puts links_array.sort.uniq
